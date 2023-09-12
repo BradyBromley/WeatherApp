f73ca864b32ff5524fe56eeeaa9f788f9fb9b561
Brady Bromley <bradybromley@live.ca>
2023-09-12 13:06:48 -0700

    Improved formatting for weather cards

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -16,12 +16,17 @@ form.addEventListener('submit', (event) => {
         success: function(response) {
             // Process Data
             console.log(response);
-            $('#appTitle').text(response["location"]["name"]);
+            id = response['location']['lat'].toString() + response['location']['lon'].toString();
+            
+            // Add the location to the list if it has not been added already
+            if (!$("#" + id.replace( /(\.)/g, "\\$1" )).length) {
+                $('#cities').append('<li class="city" id="' + id + '">' + response['location']['name'] + '</li>');
+            }
 
         },
         error: function(xhr, status, error) {
             // Handle Errors
-            $('#appTitle').text('ERROR');
+            $('#result').text('ERROR');
         }
     });
 });
